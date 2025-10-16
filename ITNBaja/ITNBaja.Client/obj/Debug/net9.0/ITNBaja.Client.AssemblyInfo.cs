3e800c2712fea6c50675fad4375aaa61c85f7120
Antipenko Denis <antipenko_dn@taxtelecom.net>
2025-10-16 20:59:20 +0300

    Removed redundant files

--- ITNBaja/ITNBaja.Client/obj/Debug/net9.0/ITNBaja.Client.AssemblyInfo.cs
+++ ITNBaja/ITNBaja.Client/obj/Debug/net9.0/ITNBaja.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITNBaja.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179354ee21ade3cf17a39ae667a2569e91991946")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITNBaja.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITNBaja.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ITNBaja/ITNBaja/obj/Debug/net9.0/ITNBaja.AssemblyInfo.cs
+++ ITNBaja/ITNBaja/obj/Debug/net9.0/ITNBaja.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITNBaja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179354ee21ade3cf17a39ae667a2569e91991946")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITNBaja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITNBaja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
